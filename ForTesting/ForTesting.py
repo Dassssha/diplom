64faa1cd2959cbeb5e4d44d235bdc499136cf701
Mikhail <49751385+Bors1k@users.noreply.github.com>
2021-04-17 14:16:56 +0300

    upd

--- ForTesting/ForTesting.py
+++ ForTesting/ForTesting.py
@@ -8,6 +8,7 @@ import adsk.core, adsk.fusion, adsk.cam, traceback, math
 def run(context):
     ui = None
     try:
+        # Переменные
         params = {"d":300, "D":620, "B":185, "r":10,"r1":7.5}
         d = params["d"]
         D = params["D"]
@@ -22,30 +23,37 @@ def run(context):
 
         CylCount = round(math.pi * (KID1 + 0.5*(KAD1-KID1)) / ((KAD1-KID1)/2)/2)
 
+        # Стандартные получалки
         app = adsk.core.Application.get()
         ui  = app.userInterface
         design = app.activeProduct
         components = design.allComponents
         rootComp = design.rootComponent
+
+        # Получаем ссылку на нужный компонент
         occurence = rootComp.occurrences.item(rootComp.occurrences.count-1)
+        # Получаем массив
+        patternFeature = occurence.component.features.circularPatternFeatures[0]
+        # Получаем скетч и его размеры
         sketches = occurence.component.sketches[0]
         sketchDimensions = sketches.sketchDimensions
-        _dim = sketchDimensions[0].expression = str(B) + " mm"
-        dim = sketchDimensions[2].parameters.expression
-        # sketchDimensions[1].expression = str(d) + " mm"
-        # sketchDimensions[2].expression = str(B) + " mm"
-        # sketchDimensions[3].expression = str(B) + " mm"
-        # sketchDimensions[4].expression = str(B) + " mm"
-        # sketchDimensions[5].expression = str(B) + " mm"
-        # sketchDimensions[6].expression = str(B) + " mm"
-        # sketchDimensions[0].expression = str(B) + " mm"
+        # Получаем коллекцию скруглений
+        filletFeatures = occurence.component.features.filletFeatures
+        
+        # Вносим новые параметры
+        sketchDimensions[0].parameter.expression = str(B) + " mm"
+        sketchDimensions[1].parameter.expression = str(d) + " mm"
+        sketchDimensions[2].parameter.expression = str(ADA) + " mm"
+        sketchDimensions[3].parameter.expression = str(KID1) + " mm"
+        sketchDimensions[4].parameter.expression = str(BID1) + " mm"
+        sketchDimensions[5].parameter.expression = str(KAD1) + " mm"
+        sketchDimensions[8].parameter.expression = str(D) + " mm"
+        
+        # Изменяем количество цилиндров
+        patternFeature.quantity.expression = str(CylCount)
 
-        # parametres = occurence.component.parentDesign.allParameters
-        # parametres.itemByName('d').expression = "50 mm"
-        # parametres.itemByName('D').expression = "80 mm"
-        # parametres.itemByName('B').expression = "16 mm"
-        # parametres.itemByName('_r').expression = "1.5 mm"
-        # parametres.itemByName('_r1').expression = "1 mm"
+        # Правим скругления
+        filletFeatures[0]
 
         u = 0
     except:
